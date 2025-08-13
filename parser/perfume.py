97b9fe3addfe52d2aafbbdc5e60a9b337a5a8a44
zemld <zemlyanukhind@yandex.ru>
2025-08-13 22:20:06 +0300

    added constructor with parameters

--- parser/perfume.py
+++ parser/perfume.py
@@ -7,3 +7,23 @@ class Perfume:
     middle_notes: list[str]
     base_notes: list[str]
     volume: list[int]
+
+    def __init__(
+        self,
+        brand: str = "",
+        name: str = "",
+        perfume_type: str = "",
+        family: str = "",
+        upper_notes: list[str] = [],
+        middle_notes: list[str] = [],
+        base_notes: list[str] = [],
+        volume: list[int] = [],
+    ):
+        self.brand = brand
+        self.name = name
+        self.perfume_type = perfume_type
+        self.family = family
+        self.upper_notes = upper_notes
+        self.middle_notes = middle_notes
+        self.base_notes = base_notes
+        self.volume = volume
