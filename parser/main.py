484f327568a51be4b0161bf57b1d53681e1c4851
zemld <zemlyanukhind@yandex.ru>
2025-08-14 17:30:31 +0300

    schedule perfum update

---
+++ parser/main.py
@@ -0,0 +1,15 @@
+from perfume import Perfume
+from url_collectors.gold_apple import collect_links_from_sitemap
+from perfume_info_collector.gold_apple import parse_pages_to_perfumes
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def update_perfumes():
+    collect_links_from_sitemap()
+    perfumes = parse_pages_to_perfumes()
+
+
+if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_perfumes, "cron", hour=3, minute=0, day_of_week="sun")
+    scheduler.start()
