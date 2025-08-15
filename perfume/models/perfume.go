b4c05e610437bbd860bb9b5326422e5887cd3677
zemld <zemlyanukhind@yandex.ru>
2025-08-15 17:32:57 +0300

    created perfume model

---
+++ perfume/models/perfume.go
@@ -0,0 +1,14 @@
+package models
+
+type Perfume struct {
+	Brand       string   `json:"brand"`
+	Name        string   `json:"name"`
+	Type        string   `json:"type"`
+	Sex         string   `json:"sex"`
+	Family      string   `json:"family"`
+	UpperNotes  []string `json:"upper_notes"`
+	MiddleNotes []string `json:"middle_notes"`
+	BaseNotes   []string `json:"base_notes"`
+	Volume      int      `json:"volume"`
+	Link        string   `json:"link"`
+}
