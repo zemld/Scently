e6ca86811dd1eb91b8b85e752a8421cc5b4bdc10
zemld <zemlyanukhind@yandex.ru>
2025-08-17 13:47:07 +0300

    config now has more info fields

--- perfume/db/config/config.go
+++ perfume/db/config/config.go
@@ -4,10 +4,25 @@ import (
 	"os"
 )
 
+type configKeyType struct{}
+
+var ConfigKey = configKeyType{}
+
 type Config struct {
-	ConnectionString string
+	User     string
+	Db       string
+	Host     string
+	Password string
 }
 
 func NewConfig() *Config {
-	return &Config{ConnectionString: os.Getenv("DB_URL")}
+	return &Config{User: os.Getenv("POSTGRES_USER"), Db: os.Getenv("POSTGRES_DB"), Host: os.Getenv("POSTGRES_HOST"), Password: getPassword()}
+}
+
+func (c *Config) GetConnectionString() string {
+	return "postgres//:" + c.User + ":" + c.Password + "@" + c.Host + ":5432/" + c.Db
+}
+
+func getPassword() string {
+	return ""
 }
