b6ad4c170011ce27cd38bbdda7f1adbe1dff0309
zemld <zemlyanukhind@yandex.ru>
2025-08-17 18:56:08 +0300

    created function for getting query and args for it

--- perfume/db/core/select-params.go
+++ perfume/db/core/select-params.go
@@ -1,5 +1,12 @@
 package core
 
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/zemld/PerfumeRecommendationSystem/perfume/db/constants"
+)
+
 type SelectParameters struct {
 	Brand string
 	Name  string
@@ -17,4 +24,29 @@ func (p *SelectParameters) WithBrand(brand string) *SelectParameters {
 func (p *SelectParameters) WithName(name string) *SelectParameters {
 	p.Name = name
 	return p
+}
+
+func (p *SelectParameters) getQuery() string {
+	query := constants.SelectQuery
+	if p.Brand != "" && p.Name != "" {
+		return fmt.Sprintf("%s WHERE brand = $1 AND name = $2", query)
+	}
+	if p.Brand != "" {
+		return fmt.Sprintf("%s WHERE brand = $1", query)
+	}
+	if p.Name != "" {
+		return fmt.Sprintf("%s WHERE name = $1", query)
+	}
+	return query
+}
+
+func (p *SelectParameters) unpack() []any {
+	var args []any
+	v := reflect.ValueOf(*p)
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).Interface() != "" {
+			args = append(args, v.Field(i).Interface())
+		}
+	}
+	return args
 }
