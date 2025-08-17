75c158200c3dbeff8e261b85e89e2714ca470102
zemld <zemlyanukhind@yandex.ru>
2025-08-17 16:56:18 +0300

    created function for updating db state

---
+++ perfume/db/core/update.go
@@ -0,0 +1,46 @@
+package core
+
+import (
+	"log"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/zemld/PerfumeRecommendationSystem/perfume/db/config"
+	"github.com/zemld/PerfumeRecommendationSystem/perfume/db/constants"
+	"github.com/zemld/PerfumeRecommendationSystem/perfume/db/internal"
+	"github.com/zemld/PerfumeRecommendationSystem/perfume/models"
+)
+
+func Update(perfumes []models.GluedPerfume) {
+	config := config.NewConfig()
+	ctx, cancel := internal.CreateContext(config)
+	defer cancel()
+
+	conn, err := pgx.Connect(ctx, config.GetConnectionString())
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(ctx)
+
+	tx, _ := conn.Begin(ctx)
+	defer tx.Rollback(ctx)
+
+	// TODO: use reflect
+	for _, perfume := range perfumes {
+		_, err = tx.Exec(ctx, constants.UpdateQuery,
+			perfume.Brand,
+			perfume.Name,
+			perfume.Type,
+			perfume.Sex,
+			perfume.Family,
+			perfume.UpperNotes,
+			perfume.MiddleNotes,
+			perfume.BaseNotes,
+			perfume.Volumes,
+			perfume.Links)
+		if err != nil {
+			log.Printf("Error updating perfume %s %s: %v\n", perfume.Brand, perfume.Name, err)
+		}
+	}
+	tx.Commit(ctx)
+	log.Println("Perfume table updated successfully")
+}
