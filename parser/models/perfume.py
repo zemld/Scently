67b9bcf9367e91bfde0b25e2321268fc07ab9f96
zemld <zemlyanukhind@yandex.ru>
2025-08-21 19:20:40 +0300

    removed glued perfume class

--- parser/models/perfume.py
+++ parser/models/perfume.py
@@ -51,43 +51,6 @@ class Perfume:
             f"\t{self._repr_property('link', self.link)}\n)\n"
         )
 
-
-class GluedPerfume(Perfume):
-    volumes: set[int]
-    links: set[str]
-
-    def __init__(self, perfume: Perfume):
-        super().__init__(
-            brand=perfume.brand,
-            name=perfume.name,
-            perfume_type=perfume.perfume_type,
-            sex=perfume.sex,
-            family=perfume.family,
-            upper_notes=perfume.upper_notes,
-            middle_notes=perfume.middle_notes,
-            base_notes=perfume.base_notes,
-        )
-        self.volumes = set()
-        self.links = set()
-        if perfume.volume:
-            self.volumes.add(perfume.volume)
-        if perfume.link:
-            self.links.add(perfume.link)
-
-    def __repr__(self):
-        return (
-            f"GluedPerfume(\n\t{self._repr_property('brand', self.brand)},\n"
-            f"\t{self._repr_property('name', self.name)},\n"
-            f"\t{self._repr_property('perfume_type', self.perfume_type)},\n"
-            f"\t{self._repr_property('sex', self.sex)},\n"
-            f"\t{self._repr_property('family', self.family)},\n"
-            f"\t{self._repr_property('upper_notes', self.upper_notes)},\n"
-            f"\t{self._repr_property('middle_notes', self.middle_notes)},\n"
-            f"\t{self._repr_property('base_notes', self.base_notes)},\n"
-            f"\t{self._repr_property('volumes', self.volumes)},\n"
-            f"\t{self._repr_property('links', self.links)}\n)\n"
-        )
-
     def to_dict(self):
         return {
             "brand": self.brand,
@@ -98,6 +61,6 @@ class GluedPerfume(Perfume):
             "upper_notes": self.upper_notes,
             "middle_notes": self.middle_notes,
             "base_notes": self.base_notes,
-            "volumes": list(self.volumes),
-            "links": list(self.links),
+            "volume": self.volume,
+            "link": self.link,
         }
