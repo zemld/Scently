cac48ccc06369f3a4077df392fa305a29da754a4
zemld <zemlyanukhind@yandex.ru>
2025-08-25 18:39:02 +0300

    added types for perfumes

---
+++ perfumist/models/perfume.go
@@ -0,0 +1,20 @@
+package models
+
+type Perfume struct {
+	Brand       string   `json:"brand"`
+	Name        string   `json:"name"`
+	Type        string   `json:"type"`
+	Sex         string   `json:"sex"`
+	Family      string   `json:"family"`
+	UpperNotes  []string `json:"upper_notes"`
+	MiddleNotes []string `json:"middle_notes"`
+	BaseNotes   []string `json:"base_notes"`
+	Link        string   `json:"link"`
+	Volume      int      `json:"volume"`
+}
+
+type GluedPerfume struct {
+	Perfume
+	Volumes []int    `json:"volumes"`
+	Links   []string `json:"links"`
+}
