02399f7ca7e7e5394cec0d05f33f8f1f2c231747
zemld <zemlyanukhind@yandex.ru>
2025-09-06 19:49:22 +0300

    created type canonizer

---
+++ parser/util/canonization/type_canonizer.py
@@ -0,0 +1,17 @@
+from util.canonization.canonizer import Canonizer
+from util.levenshtein_distance import get_levenshtein_distance
+
+
+class TypeCanonizer(Canonizer):
+    def canonize_type(self, perfume_type: str) -> str | None:
+        exact = super()._canonize_with_exact(perfume_type)
+        if exact:
+            return exact
+        lev_diff = super()._levenshtein_diff + 1
+        result = None
+        for key in self.mapping.keys():
+            lev_dist = get_levenshtein_distance(perfume_type, key)
+            if lev_dist < lev_diff:
+                lev_diff = lev_dist
+                result = self.mapping[key]
+        return result
