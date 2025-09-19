d5120fcfb28225bff5da6efb439602e58f9c5677
zemld <zemlyanukhind@yandex.ru>
2025-09-19 13:02:48 +0300

    added function to count similarity of perfumes' families

---
+++ perfumist/internal/similarity/families_similarity.go
@@ -0,0 +1,15 @@
+package similarity
+
+import "github.com/zemld/PerfumeRecommendationSystem/perfumist/pkg/set"
+
+func getFamiliesSimilarityScore(first []string, second []string) float64 {
+	firstSet := set.MakeSet(first)
+	secondSet := set.MakeSet(second)
+	intersection := set.Intersect(firstSet, secondSet)
+	un := set.Union(firstSet, secondSet)
+
+	if len(un) == 0 {
+		return 0
+	}
+	return float64(len(intersection)) / float64(len(un))
+}
