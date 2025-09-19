992038c3a9e02af6d6ec2a07d1d153bfa739227c
zemld <zemlyanukhind@yandex.ru>
2025-09-19 13:04:23 +0300

    added weights for scoring similarity

---
+++ perfumist/internal/similarity/weights.go
@@ -0,0 +1,13 @@
+package similarity
+
+const (
+	familyWeight = 0.4
+	notesWeight  = 0.55
+	typeWeight   = 0.05
+)
+
+const (
+	upperNotesWeight  = 0.15
+	middleNotesWeight = 0.45
+	baseNotesWeight   = 0.4
+)
