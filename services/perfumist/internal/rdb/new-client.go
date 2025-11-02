9c59fd8b1db42d65644d8e52be4f5986057ac1eb
zemld <zemlyanukhind@yandex.ru>
2025-11-02 09:24:37 +0300

    created env for redis password

--- services/perfumist/internal/rdb/new-client.go
+++ services/perfumist/internal/rdb/new-client.go
@@ -14,29 +14,17 @@ var (
 )
 
 const (
-	redisHost         = "redis_cache"
-	redisPort         = "6379"
-	redisPasswordFile = "REDIS_PASSWORD"
+	redisHost        = "redis_cache"
+	redisPort        = "6379"
+	redisPasswordEnv = "REDIS_PASSWORD"
 )
 
 func GetRedisClient() *redis.Client {
 	once.Do(func() {
 		client = redis.NewClient(&redis.Options{
 			Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
-			Password: getRedisPassword(),
+			Password: os.Getenv(redisPasswordEnv),
 		})
 	})
 	return client
-}
-
-func getRedisPassword() string {
-	filename := os.Getenv(redisPasswordFile)
-	if filename == "" {
-		return ""
-	}
-	password, err := os.ReadFile(filename)
-	if err != nil {
-		return ""
-	}
-	return string(password)
 }
