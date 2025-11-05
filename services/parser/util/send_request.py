abe7e72445e7283a19fad8a56c5a75b8f5c29eea
zemld <zemlyanukhind@yandex.ru>
2025-11-05 22:34:43 +0300

    fix: update pre-commit config and send_request

--- services/parser/util/send_request.py
+++ services/parser/util/send_request.py
@@ -2,7 +2,7 @@ import time
 
 import requests
 from bs4 import BeautifulSoup
-from playwright.sync_api import (  # type: ignore[import-not-found]
+from playwright.sync_api import (
     TimeoutError as PlaywrightTimeoutError,
 )
 from playwright.sync_api import sync_playwright
