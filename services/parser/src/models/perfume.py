d5a1e2bd1c4417c800a782f69184f622d8e709fe
zemld <zemlyanukhind@yandex.ru>
2025-11-06 18:23:01 +0300

    added ability to load from dict

--- services/parser/src/models/perfume.py
+++ services/parser/src/models/perfume.py
@@ -1,3 +1,6 @@
+from typing import cast
+
+
 class PerfumeFromConcreteShop:
     class PerfumeProperties:
         perfume_type: str
@@ -29,6 +32,30 @@ class PerfumeFromConcreteShop:
                 "base_notes": self.base_notes,
             }
 
+        @classmethod
+        def from_dict(
+            cls, data: dict[str, str | list[str]]
+        ) -> "PerfumeFromConcreteShop.PerfumeProperties":
+            perfume_type = data["perfume_type"]
+            family = data["family"]
+            upper_notes = data["upper_notes"]
+            middle_notes = data["middle_notes"]
+            base_notes = data["base_notes"]
+
+            assert isinstance(perfume_type, str), "perfume_type must be str"
+            assert isinstance(family, list), "family must be list[str]"
+            assert isinstance(upper_notes, list), "upper_notes must be list[str]"
+            assert isinstance(middle_notes, list), "middle_notes must be list[str]"
+            assert isinstance(base_notes, list), "base_notes must be list[str]"
+
+            return PerfumeFromConcreteShop.PerfumeProperties(
+                perfume_type=perfume_type,
+                family=family,
+                upper_notes=upper_notes,
+                middle_notes=middle_notes,
+                base_notes=base_notes,
+            )
+
     class ShopInfo:
         class VolumeWithPrices:
             volume: int
@@ -46,6 +73,24 @@ class PerfumeFromConcreteShop:
                     "price": self.cost,
                     "link": self.link,
                 }
+
+            @classmethod
+            def from_dict(
+                cls, data: dict[str, int | str]
+            ) -> "PerfumeFromConcreteShop.ShopInfo.VolumeWithPrices":
+                volume = data["volume"]
+                cost = data["price"]
+                link = data["link"]
+
+                assert isinstance(volume, int), "volume must be int"
+                assert isinstance(cost, int), "cost must be int"
+                assert isinstance(link, str), "link must be str"
+
+                return PerfumeFromConcreteShop.ShopInfo.VolumeWithPrices(
+                    volume=volume,
+                    cost=cost,
+                    link=link,
+                )
 
         shop_name: str
         shop_link: str
@@ -71,6 +116,32 @@ class PerfumeFromConcreteShop:
                 "image_url": self.image_url,
                 "volumes_with_prices": [v.to_dict() for v in self.volumes_with_prices],
             }
+
+        @classmethod
+        def from_dict(
+            cls, data: dict[str, str | list[dict[str, str | int]]]
+        ) -> "PerfumeFromConcreteShop.ShopInfo":
+            shop_name = data["shop_name"]
+            shop_link = data["shop_link"]
+            image_url = data["image_url"]
+            volumes_with_prices = data["volumes_with_prices"]
+
+            assert isinstance(shop_name, str), "shop_name must be str"
+            assert isinstance(shop_link, str), "shop_link must be str"
+            assert isinstance(image_url, str), "image_url must be str"
+            assert isinstance(
+                volumes_with_prices, list
+            ), "volumes_with_prices must be list"
+
+            return PerfumeFromConcreteShop.ShopInfo(
+                shop_name=shop_name,
+                shop_link=shop_link,
+                image_url=image_url,
+                volumes_with_prices=[
+                    PerfumeFromConcreteShop.ShopInfo.VolumeWithPrices.from_dict(v)
+                    for v in volumes_with_prices
+                ],
+            )
 
     brand: str
     name: str
@@ -116,3 +187,38 @@ class PerfumeFromConcreteShop:
             "properties": self.properties.to_dict(),
             "shop_info": self.shop_info.to_dict(),
         }
+
+    @classmethod
+    def from_dict(
+        cls,
+        data: dict[
+            str,
+            str
+            | dict[str, str | list[str]]
+            | dict[str, str | list[dict[str, str | int]]],
+        ],
+    ) -> "PerfumeFromConcreteShop":
+        brand = data["brand"]
+        name = data["name"]
+        sex = data["sex"]
+        properties = data["properties"]
+        shop_info = data["shop_info"]
+
+        assert isinstance(brand, str), "brand must be str"
+        assert isinstance(name, str), "name must be str"
+        assert isinstance(sex, str), "sex must be str"
+        assert isinstance(properties, dict), "properties must be dict"
+        assert isinstance(shop_info, dict), "shop_info must be dict"
+
+        properties_dict = cast(dict[str, str | list[str]], properties)
+        shop_info_dict = cast(dict[str, str | list[dict[str, str | int]]], shop_info)
+
+        return PerfumeFromConcreteShop(
+            brand=brand,
+            name=name,
+            sex=sex,
+            properties=PerfumeFromConcreteShop.PerfumeProperties.from_dict(
+                properties_dict
+            ),
+            shop_info=PerfumeFromConcreteShop.ShopInfo.from_dict(shop_info_dict),
+        )
