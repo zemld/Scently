cff1e34be2333b0bccf352dd697b14db2065200e
zemld <zemlyanukhind@yandex.ru>
2025-11-07 12:57:23 +0300

    added new perfume structure

--- services/perfume/internal/models/perfume.go
+++ services/perfume/internal/models/perfume.go
@@ -49,3 +49,32 @@ func isProperty(field reflect.StructField) bool {
 func isNotProperty(field reflect.StructField) bool {
 	return field.Name == "Link" || field.Name == "Volume" || field.Name == "Sex" || field.Name == "Brand" || field.Name == "Name"
 }
+
+type UpgradedPerfume struct {
+	Brand      string            `json:"brand"`
+	Name       string            `json:"name"`
+	Sex        string            `json:"sex"`
+	Properties PerfumeProperties `json:"properties"`
+	Shops      []ShopInfo        `json:"shops"`
+}
+
+type PerfumeProperties struct {
+	Type       string   `json:"type"`
+	Family     []string `json:"family"`
+	UpperNotes []string `json:"upper_notes"`
+	CoreNotes  []string `json:"core_notes"`
+	BaseNotes  []string `json:"base_notes"`
+}
+
+type ShopInfo struct {
+	ShopName string           `json:"shop_name"`
+	Domain   string           `json:"domain"`
+	ImageUrl string           `json:"image_url"`
+	Variants []PerfumeVariant `json:"variants"`
+}
+
+type PerfumeVariant struct {
+	Volume int    `json:"volume"`
+	Link   string `json:"link"`
+	Price  int    `json:"price"`
+}
