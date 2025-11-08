ca20de072e7f27b453d1205057c3e0746c9d5450
zemld <zemlyanukhind@yandex.ru>
2025-11-08 15:08:33 +0300

    updated parameters tests

--- services/perfume/internal/models/params_test.go
+++ services/perfume/internal/models/params_test.go
@@ -5,50 +5,212 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/zemld/PerfumeRecommendationSystem/perfume/internal/db/constants"
+	queries "github.com/zemld/PerfumeRecommendationSystem/perfume/internal/db/query"
 )
 
-func TestSelectParameters_getQuery(t *testing.T) {
-	baseQuery := strings.TrimSpace(constants.Select)
-	groupByIndex := strings.LastIndex(baseQuery, "GROUP BY")
+func TestSelectParameters_GetChoosingPerfumesQuery(t *testing.T) {
+	baseQuery := strings.TrimSpace(queries.PerfumesBaseInfo)
 
 	tests := []struct {
-		name     string
-		p        *SelectParameters
-		want     string
-		wantArgs []any
+		name string
+		p    *SelectParameters
+		want string
 	}{
-		{"no filters", NewSelectParameters(),
-			baseQuery[:groupByIndex] + " WHERE s.sex = 'unisex' " + baseQuery[groupByIndex:], nil},
-		{"brand only", NewSelectParameters().WithBrand("Chanel"),
-			baseQuery[:groupByIndex] + " WHERE pb.brand = $1 AND s.sex = 'unisex' " + baseQuery[groupByIndex:], []any{"Chanel"}},
-		{"name only", NewSelectParameters().WithName("No.5"),
-			baseQuery[:groupByIndex] + " WHERE pb.name = $1 AND s.sex = 'unisex' " + baseQuery[groupByIndex:], []any{"No.5"}},
-		{"sex unisex", NewSelectParameters().WithSex("unisex"),
-			baseQuery[:groupByIndex] + " WHERE s.sex = 'unisex' " + baseQuery[groupByIndex:], nil},
-		{"sex female", NewSelectParameters().WithSex("female"),
-			baseQuery[:groupByIndex] + " WHERE (s.sex = $1 OR s.sex = 'unisex') " + baseQuery[groupByIndex:], []any{"female"}},
-		{"sex male", NewSelectParameters().WithSex("male"),
-			baseQuery[:groupByIndex] + " WHERE (s.sex = $1 OR s.sex = 'unisex') " + baseQuery[groupByIndex:], []any{"male"}},
-		{"brand and name", NewSelectParameters().WithBrand("Dior").WithName("Sauvage"),
-			baseQuery[:groupByIndex] + " WHERE pb.brand = $1 AND pb.name = $2 AND s.sex = 'unisex' " + baseQuery[groupByIndex:], []any{"Dior", "Sauvage"}},
-		{"brand and sex", NewSelectParameters().WithBrand("Chanel").WithSex("female"),
-			baseQuery[:groupByIndex] + " WHERE pb.brand = $1 AND (s.sex = $2 OR s.sex = 'unisex') " + baseQuery[groupByIndex:], []any{"Chanel", "female"}},
-		{"name and sex unisex", NewSelectParameters().WithName("No.5").WithSex("unisex"),
-			baseQuery[:groupByIndex] + " WHERE pb.name = $1 AND s.sex = 'unisex' " + baseQuery[groupByIndex:], []any{"No.5"}},
-		{"name and sex female", NewSelectParameters().WithName("No.5").WithSex("female"),
-			baseQuery[:groupByIndex] + " WHERE pb.name = $1 AND (s.sex = $2 OR s.sex = 'unisex') " + baseQuery[groupByIndex:], []any{"No.5", "female"}},
-		{"brand, name and sex", NewSelectParameters().WithBrand("Dior").WithName("Sauvage").WithSex("male"),
-			baseQuery[:groupByIndex] + " WHERE pb.brand = $1 AND pb.name = $2 AND (s.sex = $3 OR s.sex = 'unisex') " + baseQuery[groupByIndex:], []any{"Dior", "Sauvage", "male"}},
+		{
+			"no filters - defaults to unisex",
+			NewSelectParameters(),
+			baseQuery + " WHERE s.sex = 'unisex'",
+		},
+		{
+			"brand only",
+			NewSelectParameters().WithBrand("Chanel"),
+			baseQuery + " WHERE pb.brand = $1 AND s.sex = 'unisex'",
+		},
+		{
+			"name only",
+			NewSelectParameters().WithName("No.5"),
+			baseQuery + " WHERE pb.name = $1 AND s.sex = 'unisex'",
+		},
+		{
+			"sex unisex",
+			NewSelectParameters().WithSex("unisex"),
+			baseQuery + " WHERE s.sex = 'unisex'",
+		},
+		{
+			"sex female",
+			NewSelectParameters().WithSex("female"),
+			baseQuery + " WHERE (s.sex = $1 OR s.sex = 'unisex')",
+		},
+		{
+			"sex male",
+			NewSelectParameters().WithSex("male"),
+			baseQuery + " WHERE (s.sex = $1 OR s.sex = 'unisex')",
+		},
+		{
+			"brand and name",
+			NewSelectParameters().WithBrand("Dior").WithName("Sauvage"),
+			baseQuery + " WHERE pb.brand = $1 AND pb.name = $2 AND s.sex = 'unisex'",
+		},
+		{
+			"brand and sex female",
+			NewSelectParameters().WithBrand("Chanel").WithSex("female"),
+			baseQuery + " WHERE pb.brand = $1 AND (s.sex = $2 OR s.sex = 'unisex')",
+		},
+		{
+			"brand and sex unisex",
+			NewSelectParameters().WithBrand("Chanel").WithSex("unisex"),
+			baseQuery + " WHERE pb.brand = $1 AND s.sex = 'unisex'",
+		},
+		{
+			"name and sex unisex",
+			NewSelectParameters().WithName("No.5").WithSex("unisex"),
+			baseQuery + " WHERE pb.name = $1 AND s.sex = 'unisex'",
+		},
+		{
+			"name and sex female",
+			NewSelectParameters().WithName("No.5").WithSex("female"),
+			baseQuery + " WHERE pb.name = $1 AND (s.sex = $2 OR s.sex = 'unisex')",
+		},
+		{
+			"brand, name and sex male",
+			NewSelectParameters().WithBrand("Dior").WithName("Sauvage").WithSex("male"),
+			baseQuery + " WHERE pb.brand = $1 AND pb.name = $2 AND (s.sex = $3 OR s.sex = 'unisex')",
+		},
+		{
+			"brand, name and sex unisex",
+			NewSelectParameters().WithBrand("Dior").WithName("Sauvage").WithSex("unisex"),
+			baseQuery + " WHERE pb.brand = $1 AND pb.name = $2 AND s.sex = 'unisex'",
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.p.GetQuery(); got != tt.want {
-				t.Fatalf("getQuery() = %q, want %q", got, tt.want)
+			got := tt.p.GetChoosingPerfumesQuery()
+			if got != tt.want {
+				t.Errorf("GetChoosingPerfumesQuery() = %q, want %q", got, tt.want)
 			}
-			if gotArgs := tt.p.Unpack(); !reflect.DeepEqual(gotArgs, tt.wantArgs) {
-				t.Fatalf("unpack() = %#v, want %#v", gotArgs, tt.wantArgs)
+		})
+	}
+}
+
+func TestSelectParameters_Unpack(t *testing.T) {
+	tests := []struct {
+		name     string
+		p        *SelectParameters
+		wantArgs []any
+	}{
+		{
+			"no filters",
+			NewSelectParameters(),
+			nil,
+		},
+		{
+			"brand only",
+			NewSelectParameters().WithBrand("Chanel"),
+			[]any{"Chanel"},
+		},
+		{
+			"name only",
+			NewSelectParameters().WithName("No.5"),
+			[]any{"No.5"},
+		},
+		{
+			"sex unisex - no args",
+			NewSelectParameters().WithSex("unisex"),
+			nil,
+		},
+		{
+			"sex female",
+			NewSelectParameters().WithSex("female"),
+			[]any{"female"},
+		},
+		{
+			"sex male",
+			NewSelectParameters().WithSex("male"),
+			[]any{"male"},
+		},
+		{
+			"brand and name",
+			NewSelectParameters().WithBrand("Dior").WithName("Sauvage"),
+			[]any{"Dior", "Sauvage"},
+		},
+		{
+			"brand and sex female",
+			NewSelectParameters().WithBrand("Chanel").WithSex("female"),
+			[]any{"Chanel", "female"},
+		},
+		{
+			"brand and sex unisex - no sex arg",
+			NewSelectParameters().WithBrand("Chanel").WithSex("unisex"),
+			[]any{"Chanel"},
+		},
+		{
+			"name and sex female",
+			NewSelectParameters().WithName("No.5").WithSex("female"),
+			[]any{"No.5", "female"},
+		},
+		{
+			"brand, name and sex male",
+			NewSelectParameters().WithBrand("Dior").WithName("Sauvage").WithSex("male"),
+			[]any{"Dior", "Sauvage", "male"},
+		},
+		{
+			"brand, name and sex unisex - no sex arg",
+			NewSelectParameters().WithBrand("Dior").WithName("Sauvage").WithSex("unisex"),
+			[]any{"Dior", "Sauvage"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotArgs := tt.p.Unpack()
+			if !reflect.DeepEqual(gotArgs, tt.wantArgs) {
+				t.Errorf("Unpack() = %#v, want %#v", gotArgs, tt.wantArgs)
+			}
+		})
+	}
+}
+
+func TestSelectParameters_GetQuery(t *testing.T) {
+	tests := []struct {
+		name string
+		p    *SelectParameters
+		want string
+	}{
+		{
+			"no filters - defaults to unisex",
+			NewSelectParameters(),
+			func() string {
+				choosingQuery := strings.TrimSpace(queries.PerfumesBaseInfo) + " WHERE s.sex = 'unisex'"
+				withClause := strings.Replace(queries.With, "%s", choosingQuery, 1)
+				return withClause + queries.EnrichSelectedPerfumes
+			}(),
+		},
+		{
+			"with brand filter",
+			NewSelectParameters().WithBrand("Chanel"),
+			func() string {
+				choosingQuery := strings.TrimSpace(queries.PerfumesBaseInfo) + " WHERE pb.brand = $1 AND s.sex = 'unisex'"
+				withClause := strings.Replace(queries.With, "%s", choosingQuery, 1)
+				return withClause + queries.EnrichSelectedPerfumes
+			}(),
+		},
+		{
+			"with sex filter",
+			NewSelectParameters().WithSex("female"),
+			func() string {
+				choosingQuery := strings.TrimSpace(queries.PerfumesBaseInfo) + " WHERE (s.sex = $1 OR s.sex = 'unisex')"
+				withClause := strings.Replace(queries.With, "%s", choosingQuery, 1)
+				return withClause + queries.EnrichSelectedPerfumes
+			}(),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.p.GetQuery()
+			if got != tt.want {
+				t.Errorf("GetQuery() = %q, want %q", got, tt.want)
 			}
 		})
 	}
