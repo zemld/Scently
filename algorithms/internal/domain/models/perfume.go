9ca36678697f680eadde06ac5d5a77d6efe6ec9f
zemld <zemlyanukhind@yandex.ru>
2025-11-23 22:47:00 +0300

    updated perfume model

---
+++ algorithms/internal/domain/models/perfume.go
@@ -0,0 +1,97 @@
+package models
+
+type Perfume struct {
+	Brand      string     `json:"brand"`
+	Name       string     `json:"name"`
+	Sex        string     `json:"sex"`
+	ImageUrl   string     `json:"image_url"`
+	Properties Properties `json:"properties"`
+	Shops      []ShopInfo `json:"shops"`
+}
+
+type Properties struct {
+	Type   string   `json:"perfume_type"`
+	Family []string `json:"family"`
+
+	UpperNotes []string `json:"upper_notes"`
+	CoreNotes  []string `json:"core_notes"`
+	BaseNotes  []string `json:"base_notes"`
+
+	EnrichedUpperNotes []EnrichedNote
+	EnrichedCoreNotes  []EnrichedNote
+	EnrichedBaseNotes  []EnrichedNote
+
+	UpperTags map[string]int
+	CoreTags  map[string]int
+	BaseTags  map[string]int
+
+	Tags map[string]int
+
+	UpperCharacteristics map[string]float64
+	CoreCharacteristics  map[string]float64
+	BaseCharacteristics  map[string]float64
+
+	Characteristics map[string]float64
+}
+
+type EnrichedNote struct {
+	Name            string
+	Tags            map[string]int
+	Characteristics map[string]float64
+}
+
+type ShopInfo struct {
+	ShopName string    `json:"shop_name"`
+	Domain   string    `json:"domain"`
+	ImageUrl string    `json:"image_url,omitempty"`
+	Variants []Variant `json:"variants"`
+}
+
+type Variant struct {
+	Volume int    `json:"volume"`
+	Link   string `json:"link"`
+	Price  int    `json:"price"`
+}
+
+func (p Perfume) Equal(other Perfume) bool {
+	return p.Brand == other.Brand && p.Name == other.Name && p.Sex == other.Sex
+}
+
+func UniteTags(notes []EnrichedNote) map[string]int {
+	united := make(map[string]int)
+
+	for _, note := range notes {
+		for tag, count := range note.Tags {
+			united[tag] += count
+		}
+	}
+
+	return united
+}
+
+func UniteCharacteristics(notes []EnrichedNote) map[string]float64 {
+	united := make(map[string]float64)
+
+	for _, note := range notes {
+		for characteristic, value := range note.Characteristics {
+			united[characteristic] += value
+		}
+	}
+
+	for characteristic, value := range united {
+		united[characteristic] = value / float64(len(notes))
+	}
+
+	return united
+}
+
+type Ranked struct {
+	Perfume Perfume `json:"perfume"`
+	Rank    int     `json:"rank,omitempty"`
+	Score   float64 `json:"similarity_score,omitempty"`
+}
+
+type WithScore struct {
+	Perfume
+	Score float64
+}
